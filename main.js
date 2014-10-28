d29aedc42e9eca76e909382f56600c7f36cb17ed
John <john-olivierbarry@hotmail.com>
2014-10-28 16:19:02 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+
+define(function (require, exports, module) {
+    "use strict";
+    var commandName         = "newHtml";
+    var Menus               = brackets.getModule("command/Menus"),
+        CommandManager      = brackets.getModule("command/CommandManager"),
+        EditorManager       = brackets.getModule("editor/EditorManager");
+    
+    var template
+    /*
+     * Implementation
+     */
+    function loadhtml() {
+        template = require('text!html/html-template.html');
+      EditorManager.getCurrentFullEditor()._codeMirror.setValue(template);
+    }
+    /*
+     * Command
+     */
+CommandManager.register("New html", commandName, loadhtml); 
+    /*
+     * Custom menu
+     */
+var menu = Menus.addMenu("html", "edgedocks.custom.menu" );  
+ menu.addMenuItem(commandName)
+});
+
+
